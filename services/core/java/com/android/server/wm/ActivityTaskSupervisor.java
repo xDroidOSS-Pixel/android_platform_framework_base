b13cd36cf333757c80f400a9f09c76e8101ec7d0
minaripenguin <minaripenguin@users.noreply.github.com>
2023-08-18 12:30:01 +0800

    services: Implement service rescheduling API
    Change-Id: I834444010ba3cbe109dfc3555d9ecb11fe9c8ce6
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- services/core/java/com/android/server/wm/ActivityTaskSupervisor.java
+++ services/core/java/com/android/server/wm/ActivityTaskSupervisor.java
@@ -314,7 +314,7 @@ public class ActivityTaskSupervisor implements RecentTasks.Callbacks {
      * Cached value of the topmost resumed activity in the system. Updated when new activity is
      * resumed.
      */
-    private ActivityRecord mTopResumedActivity;
+    public ActivityRecord mTopResumedActivity;
 
     /**
      * Flag indicating whether we're currently waiting for the previous top activity to handle the
@@ -1881,6 +1881,10 @@ public class ActivityTaskSupervisor implements RecentTasks.Callbacks {
         checkReadyForSleepLocked(false /* allowDelay */);
 
         return timedout;
+    }
+
+    public ActivityRecord getTopResumedActivity() {
+        return mTopResumedActivity;
     }
 
     void comeOutOfSleepIfNeededLocked() {
